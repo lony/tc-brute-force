b6553e2f10ec14383f4204f8780c64536dd0b7df
Goetz Epperlein <goetz.epperlein@gmx.de>
2013-12-20 01:57:39 +0800

    Refectored following J.F. Sebastian using generators

--- TrueCryptBruteForceProcesses.py
+++ TrueCryptBruteForceProcesses.py
@@ -9,7 +9,9 @@ http://stackoverflow.com/questions/11996632/multiprocessing-in-python-while-limi
 http://stackoverflow.com/questions/20627714/python-multiprocessing-with-subprocess-stops-working
 '''
 
-import subprocess, os, sys, time, multiprocessing, Queue, itertools
+import itertools, sys, time, multiprocessing
+from subprocess import call
+from multiprocessing.dummy import Pool  # use threads
 
 word_list = [
 "foo",
@@ -21,15 +23,19 @@ word_list = [
 tc_file = r"C:\dev\tc-brute-force\test.pa"
 tc_prog = r"C:\Program Files\TrueCrypt\TrueCrypt.exe"
 tc_mount_letter = "z"
-verbose = 4 # as higher as more output is shown fatal=0-5=trace
-counter = 0
-number_of_workers = multiprocessing.cpu_count()*2
+verbose = 1
+number_of_workers = multiprocessing.cpu_count() * 10
 curent_time = time.time()
 
 def get_duration(starttime):
-		return time.time() - starttime
+	return time.time() - starttime
 
-def call_tc(password, event):
+def generate_passwords(wordlist):
+	for password_length in range(1, len(wordlist) + 1):  # no repeats
+		for password in itertools.permutations(wordlist, password_length):
+    			yield "".join(password)
+
+def valid_password(password):
 	command_args = [
 		tc_prog,
 		'/a',
@@ -37,72 +43,22 @@ def call_tc(password, event):
 		'/q',
 		'/v', tc_file,
 		'/l', tc_mount_letter,
-		'/p',  password,
+		'/p', password,
 	]
-		
-	child = subprocess.Popen(command_args, \
-		stderr=open(os.devnull, 'w'), \
-		stdout=open(os.devnull, 'w'))
-	result = child.communicate() # Really important to get error code!
-		
-	if verbose > 4:
-		print subprocess.list2cmdline(command_args).rstrip() + \
-			" Status out=" + str(result[0]) + \
-			" err=" + str(result[1]) + \
-			", code=" + str(child.returncode)
-
-	if child.returncode == 0:
-		event.set()
-		print "Successfully opened TrueCrypt file with '%s' at iteration %d, duration %.3fs" % (password, counter, get_duration(curent_time))
-		
-def call_tc_daemon(queue, event):
-	while True:
-		if queue.empty():
-			break
-		else:
-			password = queue.get()
-			call_tc(password, event)
+	rc = call(command_args, close_fds=True)
+    	return rc == 0, password
 
 if __name__ == '__main__':
 
-	manager = multiprocessing.Manager()
-	event = manager.Event()
-	worker = manager.Queue(number_of_workers)
+	pool = Pool(number_of_workers)
+	for i, (found, password) in enumerate(pool.imap_unordered(valid_password, generate_passwords(word_list))):
+    	 if i % 1000 == 0 or verbose == 1:  # report progress
+    	     sys.stderr.write("%15d|%15.2fs: %s\n" % (i, get_duration(curent_time), password))
+    	 if found:
+    		 print ("Successfully opened TrueCrypt file with '%s' at iteration %d, duration %.3fs" % (password, i, get_duration(curent_time)))
+    		 break
+	else:
+  		sys.exit("Finished TrueCrypt brute-force, after %d attempts, duration %.3fs" % (i, get_duration(curent_time)))
 
-	# start processes
-	pool = []
-	for i in xrange(number_of_workers):
-		process = multiprocessing.Process(target=call_tc_daemon, args=(worker, event))
-		process.start()
-		pool.append(process)
-
-	# generate permutations
-	for x in xrange(1, (len(word_list)+1) ):
-		for permutation in itertools.permutations(word_list, x):
-			
-			# shutdown if result is found
-			if event.is_set():
-				# wait till finished
-				for p in pool:
-					p.join(2)
-			
-				print "Finished TrueCrypt brute-force, after %d attempts, duration %.3fs" % (counter, get_duration(curent_time))
-				sys.exit(1)
-
-			counter += 1	
-			combination = ""
-
-			# build string from permutation
-			for i in range(0, len(permutation)):
-				combination += permutation[i]
-
-			# output progress
-			if verbose == 4 and counter%100 == 0:
-				print "%15d|%15.3fs: %s" % (counter, get_duration(curent_time), combination)
-
-			# avoid queue overload
-			while worker.qsize() > 100:
-				if verbose > 3:	print "Wait because queue is full, size=%d" % (worker.qsize)
-				time.sleep(4)			
-			
-			worker.put(combination)
+	pool.close() 
+	#pool.join() # uncomment if it is not the end
